4d6769cdfb4decf174eb5e02f829ff1d4d63e443
Joseph Maarek <josephmaarek@gmail.com>
2024-12-18 16:15:13 +0200

    Fix: add export in index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,14 @@
-export * from './Form.js';
-export * from './Field.js';
-export * from './StoreFactory.js';
-export * from './Types.js';
 export * from './ActionFactory.js';
 export * from './AsyncValidatorUtils.js';
+export * from './Field.js';
+export * from './Form.js';
+export {default as IncrContext} from './IncrContext.svelte';
+export * from './IncrContext.js';
+export {default as PrefixIdContext} from './PrefixIdContext.svelte';
+export * from './PrefixIdContext.js';
+export * from './RunesFactory.svelte.js';
 export * from './selectors.js';
+export * from './StoreFactory.js';
+export * from './Types.js';
+
+
